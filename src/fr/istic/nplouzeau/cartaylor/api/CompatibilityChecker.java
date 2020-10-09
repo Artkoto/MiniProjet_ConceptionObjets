2143624c601e193d8c76861bcac316c80b02edaa
Anthony Amiard <anthony.amiard@etudiant.univ-rennes1.fr>
2020-10-09 16:36:43 +0200

    Ajout javadoc

--- src/fr/istic/nplouzeau/cartaylor/api/CompatibilityChecker.java
+++ src/fr/istic/nplouzeau/cartaylor/api/CompatibilityChecker.java
@@ -2,10 +2,24 @@ package fr.istic.nplouzeau.cartaylor.api;
 
 import java.util.Set;
 
+/**
+ * A public type that checks compatibilities and requirements for part types.
+ * @author plouzeau
+ */
 public interface CompatibilityChecker {
 
+    /**
+     * Returns a set of part types that are incompatible with given part type.
+     * @param   reference   Part type to check
+     * @return  Set of incompatible part types (immutable)
+     */
     Set<PartType> getIncompatibilities(PartType reference);
 
+    /**
+     * Returns a set of part type required to use given part type.
+     * @param   reference   Par type to check
+     * @return  Set of required part types (immutable)
+     */
     Set<PartType> getRequirements(PartType reference);
 
 }
--- src/fr/istic/nplouzeau/cartaylor/api/Configuration.java
+++ src/fr/istic/nplouzeau/cartaylor/api/Configuration.java
@@ -3,20 +3,53 @@ package fr.istic.nplouzeau.cartaylor.api;
 
 import java.util.Set;
 
+/**
+ * A public type containing a user configuration.
+ * @author plouzeau
+ */
 public interface Configuration {
 
+    /**
+     * Tests if the configuration is complete and valid.
+     * @return  <code>true</code> if the configuration is valid, <code>false</code> otherwise.
+     */
     boolean isValid();
 
+    /**
+     * Tests if the configuration is complete, i.e. all categories have been configurated.
+     * @return  <code>true</code> if the configuration is complete, <code>false</code> otherwise.
+     */
     boolean isComplete();
 
+    /**
+     * Returns selected parts.
+     * @return  Set of selected parts (immutable)
+     */
     Set<PartType> getSelectedParts();
 
+    /**
+     * Select a part.
+     * @param   chosenPart  Part to select
+     */
     void selectPart(PartType chosenPart);
 
+    /**
+     * Returns selected part of given category.
+     * @param   category    Category
+     * @return  Selected part for given category
+     */
     PartType getSelectionForCategory(Category category);
 
+    /**
+     * Clear a category, i.e. remove selected part for this category.
+     * If no parts are selected, nothing happens.
+     * @param   categoryToClear Category to clear
+     */
     void unselectPartType(Category categoryToClear);
 
+    /**
+     * Clear all the configuration, i.e. remove all selected parts.
+     */
     void clear();
 
 }
