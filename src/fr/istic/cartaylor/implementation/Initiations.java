8e8c0b061407fc0ecb26e1c06726a8eb9ef685d1
yakoto <yao-arnaud.akoto@etudiant.univ-rennes1.fr>
2020-10-28 02:21:08 +0100

    les Initiations (ça ne sert à rien de retourner des enssembles vides)

--- src/fr/istic/cartaylor/implementation/Initiations.java
+++ src/fr/istic/cartaylor/implementation/Initiations.java
@@ -91,24 +91,14 @@ public class Initiations {
     }
 
     //les incompatibilities
-    Set<PartType> incForEg100 = new HashSet<PartType>(){{}};
-    Set<PartType> incForEg133 = new HashSet<PartType>(){{}};
-    Set<PartType> incForEg2010 = new HashSet<PartType>(){{}};
-    Set<PartType> incForEd110 = new HashSet<PartType>(){{}};
-    Set<PartType> incForEd180 = new HashSet<PartType>(){{}};
-    Set<PartType> incForEh120 = new HashSet<PartType>(){{}};
-    Set<PartType> incForTm5 = new HashSet<PartType>(){{}};
-    Set<PartType> incForTm6 = new HashSet<PartType>(){{}};
     Set<PartType> incForTa5 = new HashSet<PartType>(){{
         add(eg100);
     }};
-    Set<PartType> incForTs6 = new HashSet<PartType>(){{}};
     Set<PartType> incForTsf7 = new HashSet<PartType>(){{
         add(eg100);
         add(eg133);
         add(ed110);
     }};
-    Set<PartType> incForTc120 = new HashSet<PartType>(){{}};
     Set<PartType> incForXc = new HashSet<PartType>(){{
         add(eg210);
     }};
@@ -118,59 +108,17 @@ public class Initiations {
     Set<PartType> incForXs = new  HashSet<PartType>(){{
         add(eg100);
     }};
-    Set<PartType> incForIn = new HashSet<PartType>(){{}};
-    Set<PartType> incForIh = new HashSet<PartType>(){{}};
     Set<PartType> incForIs = new HashSet<PartType>(){{
         add(eg100);
         add(tm5);
     }};
 
-    public Set<PartType> getIncForEg100() {
-        return incForEg100;
-    }
-
-    public Set<PartType> getIncForEg133() {
-        return incForEg133;
-    }
-
-    public Set<PartType> getIncForEg2010() {
-        return incForEg2010;
-    }
-
-    public Set<PartType> getIncForEd110() {
-        return incForEd110;
-    }
-
-    public Set<PartType> getIncForEd180() {
-        return incForEd180;
-    }
-
-    public Set<PartType> getIncForEh120() {
-        return incForEh120;
-    }
-
-    public Set<PartType> getIncForTm5() {
-        return incForTm5;
-    }
-
-    public Set<PartType> getIncForTm6() {
-        return incForTm6;
-    }
-
     public Set<PartType> getIncForTa5() {
         return incForTa5;
     }
 
-    public Set<PartType> getIncForTs6() {
-        return incForTs6;
-    }
-
     public Set<PartType> getIncForTsf7() {
         return incForTsf7;
-    }
-
-    public Set<PartType> getIncForTc120() {
-        return incForTc120;
     }
 
     public Set<PartType> getIncForXc() {
@@ -185,112 +133,34 @@ public class Initiations {
         return incForXs;
     }
 
-    public Set<PartType> getIncForIn() {
-        return incForIn;
-    }
-
-    public Set<PartType> getIncForIh() {
-        return incForIh;
-    }
-
     public Set<PartType> getIncForIs() {
         return incForIs;
     }
 
     //les requirements
-    Set<PartType> reqForEg100 = new HashSet<PartType>(){{}};
-    Set<PartType> reqForEg133 = new HashSet<PartType>(){{}};
-    Set<PartType> reqForEg2010 = new HashSet<PartType>(){{}};
-    Set<PartType> reqForEd110 = new HashSet<PartType>(){{}};
-    Set<PartType> reqForEd180 = new HashSet<PartType>(){{}};
     Set<PartType> reqForEh120 = new HashSet<PartType>(){{
         add(tc120);
     }};
-    Set<PartType> reqForTm5 = new HashSet<PartType>(){{}};
-    Set<PartType> reqForTm6 = new HashSet<PartType>(){{}};
-    Set<PartType> reqForTa5 = new HashSet<PartType>(){{}};
-    Set<PartType> reqForTs6 = new HashSet<PartType>(){{}};
-    Set<PartType> reqForTsf7 = new HashSet<PartType>(){{}};
-    Set<PartType> reqForTc120 = new HashSet<PartType>(){{
+   Set<PartType> reqForTc120 = new HashSet<PartType>(){{
         add(eh120);
     }};
-    Set<PartType> reqForXc = new HashSet<PartType>(){{}};
-    Set<PartType> reqForXm = new HashSet<PartType>(){{}};
     Set<PartType> reqForXs = new  HashSet<PartType>(){{
         add(is);
     }};
-    Set<PartType> reqForIn = new HashSet<PartType>(){{}};
-    Set<PartType> reqForIh = new HashSet<PartType>(){{}};
-    Set<PartType> reqForIs = new HashSet<PartType>(){{
+   Set<PartType> reqForIs = new HashSet<PartType>(){{
         add(xs);
     }};
 
-    public Set<PartType> getReqForEg100() {
-        return reqForEg100;
-    }
-
-    public Set<PartType> getReqForEg133() {
-        return reqForEg133;
-    }
-
-    public Set<PartType> getReqForEg2010() {
-        return reqForEg2010;
-    }
-
-    public Set<PartType> getReqForEd110() {
-        return reqForEd110;
-    }
-
-    public Set<PartType> getReqForEd180() {
-        return reqForEd180;
-    }
-
     public Set<PartType> getReqForEh120() {
         return reqForEh120;
-    }
-
-    public Set<PartType> getReqForTm5() {
-        return reqForTm5;
-    }
-
-    public Set<PartType> getReqForTm6() {
-        return reqForTm6;
-    }
-
-    public Set<PartType> getReqForTa5() {
-        return reqForTa5;
-    }
-
-    public Set<PartType> getReqForTs6() {
-        return reqForTs6;
-    }
-
-    public Set<PartType> getReqForTsf7() {
-        return reqForTsf7;
     }
 
     public Set<PartType> getReqForTc120() {
         return reqForTc120;
     }
 
-    public Set<PartType> getReqForXc() {
-        return reqForXc;
-    }
-
-    public Set<PartType> getReqForXm() {
-        return reqForXm;
-    }
-
     public Set<PartType> getReqForXs() {
         return reqForXs;
-    }
-
-    public Set<PartType> getReqForIn() {
-        return reqForIn;
-    }
-
-    public Set<PartType> getReqForIh() {
-        return reqForIh;
     }
 
     public Set<PartType> getReqForIs() {
