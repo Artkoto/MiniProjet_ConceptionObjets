ed2e7d9ceb8fda03110e29fecd42927bb4311151
Anthony Amiard <anthony.amiard@etudiant.univ-rennes1.fr>
2020-12-15 16:27:24 +0100

    Refactoring de CompatibilityManager

--- src/fr/istic/cartaylor/api/CompatibilityManager.java
+++ src/fr/istic/cartaylor/api/CompatibilityManager.java
@@ -10,9 +10,18 @@ public interface CompatibilityManager extends CompatibilityChecker {
 
     /**
      * Add a set of incompatible part types to given part type.
-     * The incompatibility is set in both direction for each reference-target association.
+     * The incompatibility is set in both direction for each reference-target
+     * association.
+     *
+     * If compatibilities already exist for given PartType, new compatibilities
+     * are added to existing ones.
+     *
+     * An incompatibility cannot be a requirement.
+     * A incompatible part type cannot be from the same category as reference.
+     *
      * @param   reference   A part type
-     * @param   target      All incompatible part types with <code>reference</code>
+     * @param   target      All incompatible part types with
+     *                      <code>reference</code>
      */
     void addIncompatibilities(PartType reference, Set<PartType> target);
 
@@ -25,8 +34,14 @@ public interface CompatibilityManager extends CompatibilityChecker {
 
     /**
      * Add a set of required part types to use with given part type.
-     * This means that <code>reference</code> needs all part types into <code>target</code> to be used.
-     * It does not specify if each part type into <code>target</code> needs <code>reference</code> to be used, or not.
+     * This means that <code>reference</code> needs all part types into
+     * <code>target</code> to be used.
+     * It does not specify if each part type into <code>target</code> needs
+     * <code>reference</code> to be used, or not.
+     *
+     * A requirement cannot be an incompatibility.
+     * A required part type cannot be from the same category as reference.
+     *
      * @param   reference   Part type to use
      * @param   target      All required part types
      */
@@ -34,8 +49,10 @@ public interface CompatibilityManager extends CompatibilityChecker {
 
     /**
      * Remove a required part type for a given part type.
-     * This means that <code>reference</code> does not longer needs <code>target</code> to be used.
-     * It does not specify if <code>target</code> needs <code>reference</code> to be used, or not.
+     * This means that <code>reference</code> does not longer needs
+     * <code>target</code> to be used.
+     * It does not specify if <code>target</code> needs <code>reference</code>
+     * to be used, or not.
      * @param   reference   Part type to use
      * @param   target      No longer required part type
      */
