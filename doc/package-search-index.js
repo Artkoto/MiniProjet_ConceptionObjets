66c1dde673a929008de5f0ca79e2d6b671f1950c
Anthony Amiard <anthony.amiard@etudiant.univ-rennes1.fr>
2020-12-18 15:28:33 +0100

    added generated doc

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"fr.istic.cartaylor.api"},{"l":"fr.istic.cartaylor.implementation"},{"l":"fr.istic.cartaylor.test"}];updateSearchResults();
